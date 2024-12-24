079119868014a98591956f9115c4b905b9dffde0
Hafizh <Hafizh@gmail.com>
2024-12-24 15:35:13 +0700

    menambahkan assets, struktur folder semi clean architecture, pexels docs, membuat api keys dan generate api dari pexels, setup model

---
+++ lib/data/models/photo_model.dart
@@ -0,0 +1,57 @@
+import 'package:flutter_photo_idea_app/data/models/source_model.dart';
+
+class PhotoModel {
+  final int? id;
+  final int? width;
+  final int? height;
+  final String? url;
+  final String? photographer;
+  final String? photographerUrl;
+  final int? photographerId;
+  final String? avgColor;
+  final SourceModel? source;
+  final bool? liked;
+  final String? alt;
+
+  PhotoModel({
+    this.id,
+    this.width,
+    this.height,
+    this.url,
+    this.photographer,
+    this.photographerUrl,
+    this.photographerId,
+    this.avgColor,
+    this.source,
+    this.liked,
+    this.alt,
+  });
+
+  factory PhotoModel.fromJson(Map<String, dynamic> json) => PhotoModel(
+        id: json["id"],
+        width: json["width"],
+        height: json["height"],
+        url: json["url"],
+        photographer: json["photographer"],
+        photographerUrl: json["photographer_url"],
+        photographerId: json["photographer_id"],
+        avgColor: json["avg_color"],
+        source: json["src"] == null ? null : SourceModel.fromJson(json["src"]),
+        liked: json["liked"],
+        alt: json["alt"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "width": width,
+        "height": height,
+        "url": url,
+        "photographer": photographer,
+        "photographer_url": photographerUrl,
+        "photographer_id": photographerId,
+        "avg_color": avgColor,
+        "src": source?.toJson(),
+        "liked": liked,
+        "alt": alt,
+      };
+}
---
+++ lib/data/models/source_model.dart
@@ -0,0 +1,43 @@
+class SourceModel {
+  final String? original;
+  final String? large2X;
+  final String? large;
+  final String? medium;
+  final String? small;
+  final String? portrait;
+  final String? landscape;
+  final String? tiny;
+
+  SourceModel({
+    this.original,
+    this.large2X,
+    this.large,
+    this.medium,
+    this.small,
+    this.portrait,
+    this.landscape,
+    this.tiny,
+  });
+
+  factory SourceModel.fromJson(Map<String, dynamic> json) => SourceModel(
+        original: json["original"],
+        large2X: json["large2x"],
+        large: json["large"],
+        medium: json["medium"],
+        small: json["small"],
+        portrait: json["portrait"],
+        landscape: json["landscape"],
+        tiny: json["tiny"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "original": original,
+        "large2x": large2X,
+        "large": large,
+        "medium": medium,
+        "small": small,
+        "portrait": portrait,
+        "landscape": landscape,
+        "tiny": tiny,
+      };
+}
